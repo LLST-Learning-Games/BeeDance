923935af92174d8897ecaeecad77d039436e6aaf
Ben Blanchard <34079352+Rhevik@users.noreply.github.com>
2025-08-24 12:05:25 -0700

    Made walls a prefab

--- DancingBee/BeeMouseInput.cs
+++ DancingBee/BeeMouseInput.cs
@@ -3,51 +3,51 @@ using System;
 
 public partial class BeeMouseInput : Node2D
 {
-    [Export] private Node2D _beeButt; 
-    [Export] private float _distanceRatio = 3f;
-    [Export] private float _maxInputDistance = 3f;
-    [Export] private float _maxDistance = 3f;
-    private bool _isMouseButtonDown;
-    
-    public override void _Input(InputEvent @event)
-    {
-        if (@event is InputEventMouseButton mouseButtonEvent)
-        {
-            if (mouseButtonEvent.ButtonIndex == MouseButton.Left)
-            {
-                // this means mouse button was just lifted
-                if (_isMouseButtonDown && !mouseButtonEvent.Pressed)
-                {
-                    _beeButt.Position = Vector2.Zero;
-                    Map.Instance.SpawnBeeAndMove();
-                }
-                _isMouseButtonDown = mouseButtonEvent.Pressed;
-            }
-        }
+	[Export] private Node2D _beeButt; 
+	[Export] private float _distanceRatio = 3f;
+	[Export] private float _maxInputDistance = 3f;
+	[Export] private float _maxDistance = 3f;
+	private bool _isMouseButtonDown;
+	
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton mouseButtonEvent)
+		{
+			if (mouseButtonEvent.ButtonIndex == MouseButton.Left)
+			{
+				// this means mouse button was just lifted
+				if (_isMouseButtonDown && !mouseButtonEvent.Pressed)
+				{
+					_beeButt.Position = Vector2.Zero;
+					Map.Instance.SpawnBeeAndMove();
+				}
+				_isMouseButtonDown = mouseButtonEvent.Pressed;
+			}
+		}
 
-        if (!_isMouseButtonDown)
-        {
-            return;
-        }
-        
-        if (@event is InputEventMouseMotion eventMouseMotion)
-        {
-            var relativePosition = eventMouseMotion.Position - GlobalPosition;
-            
-            if(relativePosition.Length() > _maxInputDistance)
-            {   
-                relativePosition = relativePosition.Normalized() * _maxInputDistance;
-            }
-            
-            relativePosition /= _distanceRatio;
-            
-            if(relativePosition.Length() > _maxDistance)
-            {   
-                relativePosition = relativePosition.Normalized() * _maxDistance;
-            }
-            
-            _beeButt.Position = relativePosition;
-            MovementTrackerSystem.Instance.AddMovementSnapshot(relativePosition);
-        }
-    }
+		if (!_isMouseButtonDown)
+		{
+			return;
+		}
+		
+		if (@event is InputEventMouseMotion eventMouseMotion)
+		{
+			var relativePosition = eventMouseMotion.Position - GlobalPosition;
+			
+			if(relativePosition.Length() > _maxInputDistance)
+			{   
+				relativePosition = relativePosition.Normalized() * _maxInputDistance;
+			}
+			
+			relativePosition /= _distanceRatio;
+			
+			if(relativePosition.Length() > _maxDistance)
+			{   
+				relativePosition = relativePosition.Normalized() * _maxDistance;
+			}
+			
+			_beeButt.Position = relativePosition;
+			MovementTrackerSystem.Instance.AddMovementSnapshot(relativePosition);
+		}
+	}
 }
