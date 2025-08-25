b91b48f7939a48660f2cfb23a6cce4a7af94341a
Ben Blanchard <34079352+Rhevik@users.noreply.github.com>
2025-08-24 17:57:25 -0700

    Add not-quite-working timer logic
    Doesn't trigger an end properly, doesn't flash red (just turns). ;.;

--- EnergyGauge/EnergyGauge.cs
+++ EnergyGauge/EnergyGauge.cs
@@ -5,7 +5,13 @@ public partial class EnergyGauge : Node2D
 {
 	[Export] private Timer _energyTimer;
 	[Export] private AnimatedSprite2D _gaugeBase;
-	[Export] private AnimatedSprite2D _gaugeStick;
+	[Export] private Node2D _gaugeStickRotator;
+	[Export] private float GaugeEmptyPoint;
+	[Export] private float GaugeFullPoint;
+	[Export] private float DurationOfGame;
+	[Export] private float PercentToStartFlashing;
+	[Export] private float AnimationSpeed;
+	private double previousRotation;
 	public static EnergyGauge Instance { get; private set; }
 
 	public override void _Ready()
@@ -18,7 +24,43 @@ public partial class EnergyGauge : Node2D
 		}
 
 		Instance = this;
+		_gaugeStickRotator.RotationDegrees = GaugeFullPoint;
 		_energyTimer.Timeout += OnEnergyDepleted;
+		_energyTimer.WaitTime = DurationOfGame;
+		_energyTimer.Start();
+		previousRotation = GaugeFullPoint;
+		_gaugeBase.Frame = 0;
+	}
+
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+
+		double targeRotation = GetPercentRemainingGauge() * GetGaugeRotationRange();
+		//GD.Print("PreviousRotation: " + previousRotation.ToString() + ", Target: " + targeRotation.ToString());
+		_gaugeStickRotator.Rotate((float)Mathf.DegToRad((targeRotation - previousRotation)));
+		previousRotation = targeRotation;
+		//GD.Print("EnergyWait: " + _energyTimer.WaitTime.ToString() + "GameDuration: " + DurationOfGame.ToString());
+		//GD.Print("PercentRemaining: " + GetPercentRemainingGauge().ToString() + "PercentToStartFlashing: " + PercentToStartFlashing.ToString());
+		if (GetPercentRemainingGauge() < PercentToStartFlashing)
+		{
+			_gaugeBase.Frame = 1;
+		}
+
+	}
+
+	private double GetGaugeRotationRange()
+	{
+		return Math.Abs(GaugeFullPoint - GaugeEmptyPoint);
+	}
+
+	private double GetPercentRemainingGauge()
+	{
+		if (_energyTimer == null)
+			return -1.0;
+
+		//GD.Print("GetTimeLeft: " + GetTimeLeft().ToString() + ", waitTime: " + _energyTimer.WaitTime.ToString());
+		return (GetTimeLeft() / _energyTimer.WaitTime);
 	}
 	public double GetTimeLeft()
 	{
